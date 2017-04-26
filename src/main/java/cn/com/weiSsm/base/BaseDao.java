b6081b59faa1d3579663934f36315858ebfcd663
魏洪皓 <916952748@qq.com>
2017-04-26 13:57:01 +0800

    底层基类

---
+++ src/main/java/cn/com/weiSsm/base/BaseDao.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright &copy; 2016-2020 <a href="http://strongunion.com.cn/">StrongUnion</a> All rights reserved.
+ */
+package cn.com.weiSsm.base;
+
+/**
+ * DAO支持类实现
+ *
+ * @version 2014-05-16
+ */
+public interface BaseDao {
+
+}
